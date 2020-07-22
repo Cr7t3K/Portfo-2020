135a0d20a9ef2c4799d1c1581dffd9fe58c37dd1
Cr7t3K <cvr.contact.pro@gmail.com>
2020-07-22 16:49:52 +0200

    Ajout twig variable project page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PageRepository;
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +13,15 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(PageRepository $pageRepository) : Response
+    public function index(PageRepository $pageRepository, ProjectRepository $projectRepository) : Response
     {
         $homePage = $pageRepository->findOneBy(["name" => "index"]);
+        $projects = $projectRepository->findAll();
         dump($homePage);
+        dump($projects);
         return $this->render("home/index.html.twig", [
-            "homePage" => $homePage
+            "homePage" => $homePage,
+            "projects" => $projects,
         ]);
     }
 
